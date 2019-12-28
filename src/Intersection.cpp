0766078c10c74b3aff82a4ccc1c6544627551135
Ioannis Tornazakis <itornaza@gmail.com>
2019-12-28 19:15:53 +0200

    docs: Added video link and comments

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -89,9 +89,6 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle) {
             << " is granted entry." << std::endl;
   lck.unlock();
 
-  // FP.6b : use the methods TrafficLight::getCurrentPhase and
-  // TrafficLight::waitForGreen to block the execution until the traffic light
-  // turns green.
   while (_trafficLight.getCurrentPhase() != TrafficLightPhase::green) {
     _trafficLight.waitForGreen();
   }
@@ -116,11 +113,8 @@ void Intersection::setIsBlocked(bool isBlocked) {
 }
 
 // virtual function which is executed in a thread
-void Intersection::simulate() // using threads + promises/futures + exceptions
-{
-  // FP.6a : In Intersection.h, add a private member _trafficLight of type
-  // TrafficLight. At this position, start the simulation of _trafficLight.
-  
+// using threads + promises/futures + exceptions
+void Intersection::simulate() {
   _trafficLight.simulate();
 
   // launch vehicle queue processing in a thread
@@ -149,12 +143,9 @@ void Intersection::processVehicleQueue() {
 }
 
 bool Intersection::trafficLightIsGreen() {
-  // please include this part once you have solved the final project tasks
   if (_trafficLight.getCurrentPhase() == TrafficLightPhase::green) {
     return true;
   } else {
     return false;
   }
-
-  // return true; // makes traffic light permanently green
 }
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -1,20 +1,17 @@
 #ifndef TRAFFICLIGHT_H
 #define TRAFFICLIGHT_H
 
-#include "TrafficObject.h"
 #include <condition_variable>
 #include <deque>
 #include <mutex>
 
+#include "TrafficObject.h"
+
 // forward declarations to avoid include cycle
 class Vehicle;
+enum TrafficLightPhase { red, green };
 
-// FP.3 Define a class „MessageQueue“ which has the public methods send and
-// receive. Send should take an rvalue reference of type TrafficLightPhase
-// whereas receive should return this type. Also, the class should define an
-// std::dequeue called _queue, which stores objects of type TrafficLightPhase.
-// Also, there should be an std::condition_variable as well as an std::mutex as
-// private members.
+/* Declaration of class "MessageQueue" */
 
 template <class T> class MessageQueue {
 public:
@@ -27,15 +24,7 @@ private:
   std::deque<T> _queue;
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject.
-// The class shall have the public methods „void waitForGreen()“ and „void
-// simulate()“ as well as „TrafficLightPhase getCurrentPhase()“, where
-// TrafficLightPhase is an enum that can be either „red“ or „green“. Also, add
-// the private method „void cycleThroughPhases()“. Furthermore, there shall be
-// the private member _currentPhase which can take „red“ or „green“ as its
-// value.
-
-enum TrafficLightPhase { red, green };
+/* Declaration of class "TrafficLight" */
 
 class TrafficLight : public TrafficObject {
 public:
@@ -53,11 +42,6 @@ public:
 private:
   // typical behaviour methods
   void cycleThroughPhases();
-
-  // FP.4b : create a private member of type MessageQueue for messages of type
-  // TrafficLightPhase and use it within the infinite loop to push each new
-  // TrafficLightPhase into it by calling send in conjunction with move
-  // semantics.
 
   std::shared_ptr<MessageQueue<TrafficLightPhase>> _phasesQueue;
   TrafficLightPhase _currentPhase;
